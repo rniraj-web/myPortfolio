491fdf76bcdd9ae50a66b997532ae4880e144782
Niraj Kishan Rawat <rawatn@cardiff.ac.uk>
2025-02-23 01:30:22 +0000

    added download resume functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import logo from '../assets/nirajrawatlogo1.png'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa6'
 import { FaPhone } from 'react-icons/fa'
+import { FaFileAlt } from 'react-icons/fa'
 import { HashLink } from 'react-router-hash-link'
 
 const Navbar = () => {
@@ -15,6 +16,16 @@ const Navbar = () => {
             <a href='https://www.linkedin.com/in/niraj-rawat/' target='_'><FaLinkedin/></a>
             <a href='https://github.com/rniraj-web' target='_'><FaGithub/></a>
             <HashLink to='#contact'><FaPhone/></HashLink>
+            <a 
+                href='/resume.pdf' 
+                download 
+                className="relative group text-white"
+            >
+                <FaFileAlt className="text-2xl" />
+                <span className="absolute left-1/2 -translate-x-1/2 bottom-[-30px] px-2 py-1 text-xs bg-purple-500 text-white rounded opacity-0 group-hover:opacity-70 transition">
+                    Resume
+                </span>
+            </a>
         </div>
     </nav>
   )
